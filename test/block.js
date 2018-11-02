7d98b54f6cbd36dee8119e46c47508eaf687c4b1
박정태 <pjt@bagjeongtaeui-iMac.local>
2018-11-02 13:51:25 +0900

    block 추가준비 한다

---
+++ test/block.js
@@ -0,0 +1,12 @@
+let Hycon = require('..')
+
+ // hycon 노드연결 객체 생성
+let hycon = new Hycon('http://127.0.0.1:2442');
+
+(async () =>{
+    try {
+        
+    } catch (err) {
+        console.log(err)
+    }
+})()
